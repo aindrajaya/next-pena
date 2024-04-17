903582b826270db237afc09c4af1fec1f4ff340b
pr3b <eb.underwood22@gmail.com>
2024-04-17 16:17:18 +0700

    banner card in progress

--- components/Icon.jsx
+++ components/Icon.jsx
@@ -1,12 +1,12 @@
-import { useId } from 'react'
-import clsx from 'clsx'
+import { useId } from 'react';
+import clsx from 'clsx';
 
-import { InstallationIcon } from './icons/InstallationIcon'
-import { LightbulbIcon } from './icons/LightbulbIcon'
-import { PluginsIcon } from './icons/PluginsIcon'
-import { PresetsIcon } from './icons/PresetsIcon'
-import { ThemingIcon } from './icons/ThemingIcon'
-import { WarningIcon } from './icons/WarningIcon'
+import { InstallationIcon } from './icons/InstallationIcon';
+import { LightbulbIcon } from './icons/LightbulbIcon';
+import { PluginsIcon } from './icons/PluginsIcon';
+import { PresetsIcon } from './icons/PresetsIcon';
+import { ThemingIcon } from './icons/ThemingIcon';
+import { WarningIcon } from './icons/WarningIcon';
 
 const icons = {
   installation: InstallationIcon,
@@ -15,29 +15,29 @@ const icons = {
   theming: ThemingIcon,
   lightbulb: LightbulbIcon,
   warning: WarningIcon,
-}
+};
 
 const iconStyles = {
   blue: '[--icon-foreground:theme(colors.slate.900)] [--icon-background:theme(colors.white)]',
   amber:
     '[--icon-foreground:theme(colors.amber.900)] [--icon-background:theme(colors.amber.100)]',
-}
+};
 
 export function Icon({ icon, color = 'amber', className, ...props }) {
-  let id = useId()
-  let IconComponent = icons[icon]
+  let id = useId();
+  let IconComponent = icons[icon];
 
   return (
     <svg
-      aria-hidden="true"
-      viewBox="0 0 32 32"
-      fill="none"
+      aria-hidden='true'
+      viewBox='0 0 32 32'
+      fill='none'
       className={clsx(className, iconStyles[color])}
       {...props}
     >
       <IconComponent id={id} color={color} />
     </svg>
-  )
+  );
 }
 
 const gradients = {
@@ -50,7 +50,7 @@ const gradients = {
     { stopColor: '#FDE68A', offset: '.08' },
     { stopColor: '#F59E0B', offset: '.837' },
   ],
-}
+};
 
 export function Gradient({ color = 'blue', ...props }) {
   return (
@@ -58,20 +58,20 @@ export function Gradient({ color = 'blue', ...props }) {
       cx={0}
       cy={0}
       r={1}
-      gradientUnits="userSpaceOnUse"
+      gradientUnits='userSpaceOnUse'
       {...props}
     >
       {gradients[color].map((stop, stopIndex) => (
         <stop key={stopIndex} {...stop} />
       ))}
     </radialGradient>
-  )
+  );
 }
 
 export function LightMode({ className, ...props }) {
-  return <g className={clsx('dark:hidden', className)} {...props} />
+  return <g className={clsx('dark:hidden', className)} {...props} />;
 }
 
 export function DarkMode({ className, ...props }) {
-  return <g className={clsx('hidden dark:inline', className)} {...props} />
+  return <g className={clsx('hidden dark:inline', className)} {...props} />;
 }
--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,49 +1,49 @@
-import Link from "next/link";
-import Image from "next/legacy/image";
-import { usePathname } from "next/navigation";
-import clsx from "clsx";
-import LogoPena from "../public/img/pena-text.png";
+import Link from 'next/link';
+import Image from 'next/legacy/image';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+import LogoPena from '../public/img/pena-text.png';
 
-import { navigation } from "../lib/navigation";
-import DynamicColorIcon from "./IconTail";
+import { navigation } from '../lib/navigation';
+import DynamicColorIcon from './IconTail';
 
 export function Navigation({ className, onLinkClick }) {
   let pathname = usePathname();
 
   return (
-    <div className="bg-background border-muted sidebar-width sticky top-16-screen py-8 pl-8 w-1/4 hidden lg:block overflow-y-scroll">
+    <div className='bg-background border-muted sidebar-width sticky top-16-screen py-8 pl-8 w-1/4 hidden lg:block overflow-y-scroll'>
       {/* <nav className={clsx('text-base lg:text-sm', className)}> */}
-      <div className="relative flex flex-grow basis-0 items-center pl-4 pt-2">
-        <Link href="/" aria-label="Home page">
+      <div className='relative flex flex-grow basis-0 items-center pb-8'>
+        <Link href='/' aria-label='Home page'>
           {/* <Logomark className="h-9 w-9 lg:hidden" /> */}
           {/* <Logo className="hidden h-9 w-auto fill-slate-700 lg:block dark:fill-pena-500" /> */}
-          <Image src={LogoPena} alt="Pena Logo" width={75} height={40} />
+          <Image src={LogoPena} alt='Pena Logo' width={75} height={40} />
         </Link>
       </div>
-      <ul role="list" className="space-y-9">
+      <ul role='list' className='space-y-9'>
         {navigation.map((section) => (
           <li key={section.title}>
-            <h2 className="text-s text-slate-900 dark:text-white title-font">
+            <h2 className='text-s text-slate-900 dark:text-white title-font'>
               {section.title}
             </h2>
             <ul
-              role="list"
-              className="mt-2 space-y-2 lg:mt-4 lg:space-y-4 lg:border-slate-200 dark:border-slate-800"
+              role='list'
+              className='mt-2 space-y-2 lg:mt-4 lg:space-y-4 lg:border-slate-200 dark:border-slate-800'
             >
               {section.links.map((link) => (
-                <li key={link.href} className="relative">
+                <li key={link.href} className='relative'>
                   <Link
                     href={link.href}
                     onClick={onLinkClick}
                     className={clsx(
-                      "flex w-full",
+                      'flex w-full',
                       link.href === pathname
-                        ? "font-semibold text-pena-500"
-                        : "text-slate-500 hover:text-slate-600 hover:before:block dark:text-slate-400 dark:before:bg-slate-700 dark:hover:text-slate-300"
+                        ? 'font-semibold text-pena-500'
+                        : 'text-slate-500 hover:text-slate-600 hover:before:block dark:text-slate-400 dark:before:bg-slate-700 dark:hover:text-slate-300'
                     )}
                     // className="my-5 flex items-center space-x-4"
                   >
-                    <div className="bg-surface-100 flex items-center justify-center">
+                    <div className='bg-surface-100 flex items-center justify-center'>
                       {/* <Image
                         className="rounded"
                         width={24}
@@ -55,11 +55,11 @@ export function Navigation({ className, onLinkClick }) {
                       /> */}
                       <DynamicColorIcon svg={link.icon} />
                     </div>
-                    <p className="text-foreground text-sm ml-2">{link.title}</p>
+                    <p className='text-foreground text-sm ml-2'>{link.title}</p>
                   </Link>
                 </li>
               ))}
-              <div className="h-px w-full border-b pb-1 grey-color"></div>
+              <div className='h-px w-full border-b pb-1 grey-color'></div>
             </ul>
           </li>
         ))}
