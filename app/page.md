aab6d1a5b53d57c307923738901d0fc2ca1cacd6
Arista Indrajaya <arista.indrajaya@gmail.com>
2024-04-18 00:23:06 +0700

    update minor issues

--- app/page.md
+++ app/page.md
@@ -1,5 +1,5 @@
 ---
-title: Pena Docs
+title:
 ---
 
 {% quick-links %}
--- components/QuickLinks.jsx
+++ components/QuickLinks.jsx
@@ -15,8 +15,8 @@ export function QuickLink({ title, description, href, icon }) {
     <div className='group relative rounded-xl border border-slate-200 dark:border-slate-800'>
       <div className='absolute -inset-px rounded-xl border-2 border-transparent opacity-0 [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sky.50)),var(--quick-links-hover-bg,theme(colors.sky.50)))_padding-box,linear-gradient(to_top,theme(colors.indigo.400),theme(colors.cyan.400),theme(colors.sky.500))_border-box] group-hover:opacity-100 dark:[--quick-links-hover-bg:theme(colors.slate.800)]' />
       <div className='relative overflow-hidden rounded-xl p-6'>
-        <div class='flex items-center mb-6'>
-          <div class='rounded p-2 bg-slate-800'>
+        <div className='flex items-center mb-6'>
+          <div className='rounded p-2 bg-slate-800'>
             <Icon icon={icon} className='h-6 w-6' />
           </div>
           <h2 className='ml-4 text-base text-pena-900 dark:text-pena-500'>
