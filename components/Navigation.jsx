86f80ed9d97547a7c78cf2a3eda987147e6ba5fa
pr3b <eb.underwood22@gmail.com>
2024-04-24 11:29:25 +0700

    custom-homepage

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,49 +1,48 @@
-import Link from "next/link";
-import Image from "next/legacy/image";
-import { usePathname } from "next/navigation";
-import clsx from "clsx";
-import LogoPena from "../public/img/pena-text.png";
+import Link from 'next/link';
+import Image from 'next/legacy/image';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+import LogoPena from '../public/img/pena-text.png';
 
-import { navigation } from "../lib/navigation";
-import DynamicColorIcon from "./IconTail";
+import { navigation } from '../lib/navigation';
+import DynamicColorIcon from './IconTail';
 
 export const Nav = ({ className, onLinkClick, isReferencePage, pathname }) => {
-  return(
+  return (
     <nav className={clsx('text-base lg:text-sm', className)}>
-    {
-      isReferencePage || 
-      <div className="relative flex flex-grow basis-0 items-center pl-4 pt-2 mb-12">
-        <Link href="/" aria-label="Home page">
-          {/* <Logomark className="h-9 w-9 lg:hidden" /> */}
-          {/* <Logo className="hidden h-9 w-auto fill-slate-700 lg:block dark:fill-pena-500" /> */}
-          <Image src={LogoPena} alt="Pena Logo" width={75} height={40} />
-        </Link>
-      </div>
-    }
-      <ul role="list" className="space-y-9">
+      {isReferencePage || (
+        <div className='relative flex flex-grow basis-0 items-center pl-4 pt-2 mb-12'>
+          <Link href='/' aria-label='Home page'>
+            {/* <Logomark className="h-9 w-9 lg:hidden" /> */}
+            {/* <Logo className="hidden h-9 w-auto fill-slate-700 lg:block dark:fill-pena-500" /> */}
+            <Image src={LogoPena} alt='Pena Logo' width={75} height={40} />
+          </Link>
+        </div>
+      )}
+      <ul role='list' className='space-y-9'>
         {navigation.map((section) => (
           <li key={section.title}>
-            <h2 className="text-s text-slate-900 dark:text-white title-font">
+            <h2 className='text-s text-slate-900 dark:text-white title-font'>
               {section.title}
             </h2>
             <ul
-              role="list"
-              className="mt-2 space-y-2 lg:mt-4 lg:space-y-4 lg:border-slate-200 dark:border-slate-800"
+              role='list'
+              className='mt-2 space-y-2 lg:mt-4 lg:space-y-4 lg:border-slate-200 dark:border-slate-800'
             >
               {section.links.map((link) => (
-                <li key={link.href} className="relative">
+                <li key={link.href} className='relative'>
                   <Link
                     href={link.href}
                     onClick={onLinkClick}
                     className={clsx(
-                      "flex w-full",
+                      'flex w-full',
                       link.href === pathname
-                        ? "font-semibold text-pena-500"
-                        : "text-slate-500 hover:text-slate-600 hover:before:block dark:text-slate-400 dark:before:bg-slate-700 dark:hover:text-slate-300"
+                        ? 'font-semibold text-pena-500'
+                        : 'text-slate-500 hover:text-slate-600 hover:before:block dark:text-slate-400 dark:before:bg-slate-700 dark:hover:text-slate-300'
                     )}
                     // className="my-5 flex items-center space-x-4"
                   >
-                    <div className="bg-surface-100 flex items-center justify-center">
+                    <div className='bg-surface-100 flex items-center justify-center'>
                       {/* <Image
                         className="rounded"
                         width={24}
@@ -55,26 +54,31 @@ export const Nav = ({ className, onLinkClick, isReferencePage, pathname }) => {
                       /> */}
                       <DynamicColorIcon svg={link.icon} />
                     </div>
-                    <p className="text-foreground text-sm ml-2">{link.title}</p>
+                    <p className='text-foreground text-sm ml-2'>{link.title}</p>
                   </Link>
                 </li>
               ))}
-              <div className="h-px w-full border-b pb-1 grey-color"></div>
+              <div className='h-px w-full border-b pb-1 grey-color'></div>
             </ul>
           </li>
         ))}
       </ul>
     </nav>
-  )
-}
+  );
+};
 
 export function Navigation({ className, onLinkClick }) {
   let pathname = usePathname();
-  let isReferencePage = pathname === "/reference";
+  let isReferencePage = pathname === '/reference';
 
   return (
-    <div className="bg-background border-muted sidebar-width sticky top-16-screen py-8 pl-8 w-1/4 hidden lg:block overflow-y-scroll">
-      <Nav className={className} onLinkClick={onLinkClick} isReferencePage={isReferencePage} pathname={pathname}/>
+    <div className='bg-background border-muted sidebar-width sticky top-16-screen py-8 pl-8 w-1/4 hidden lg:block overflow-y-scroll'>
+      <Nav
+        className={className}
+        onLinkClick={onLinkClick}
+        isReferencePage={isReferencePage}
+        pathname={pathname}
+      />
     </div>
   );
 }
--- components/QuickLinks.jsx
+++ components/QuickLinks.jsx
@@ -1,41 +1,65 @@
 import Link from 'next/link';
-
 import { Icon } from './Icon';
-import { Bolt, BadgeCheck, Terminal, Pointer, PenTool, Play, Users, Pyramid } from 'lucide-react';
-
-
-
+import {
+  Bolt,
+  BadgeCheck,
+  Terminal,
+  Pointer,
+  PenTool,
+  Play,
+  Users,
+  Pyramid,
+  HardDriveDownload,
+  BookMarked,
+  BookType,
+  HeartHandshake,
+  UserCog,
+} from 'lucide-react';
 
 export function QuickLinks({ children }) {
   return (
-    <div className='not-prose my-12 grid grid-cols-1 gap-6 sm:grid-cols-3'>
-      {children}
+    <div>
+      <p className='text-xl font-bold'>Getting Started</p>
+      <div className='not-prose my-12 grid grid-cols-1 gap-6 sm:grid-cols-3'>
+        {children}
+      </div>
     </div>
   );
 }
 
 export function QuickLink({ title, description, href, icon }) {
+  const getIconComponent = (icon) => {
+    switch (icon) {
+      case 'cursor':
+        return <Play />;
+      case 'pen':
+        return <PenTool />;
+      case 'terminal':
+        return <BookMarked />;
+      case 'user':
+        return <UserCog />;
+      case 'bold':
+        return <HeartHandshake />;
+      case 'arch':
+        return <BookType />;
+      case 'start':
+        return <BadgeCheck />;
+      default:
+        return <HardDriveDownload />;
+    }
+  };
+
   return (
     <div className='group relative rounded-xl border border-slate-200 dark:border-slate-800'>
-      {/* <div className='absolute -inset-px rounded-xl border-2 border-transparent opacity-0 [background:linear-gradient(var(--quick-links-hover-bg,theme(colors.sky.50)),var(--quick-links-hover-bg,theme(colors.sky.50)))_padding-box,linear-gradient(to_top,theme(colors.indigo.400),theme(colors.cyan.400),theme(colors.sky.500))_border-box] group-hover:opacity-100 dark:[--quick-links-hover-bg:theme(colors.slate.800)]' /> */}
       <div className='relative overflow-hidden rounded-xl p-6'>
         <div className='flex items-center mb-6'>
-          <div className='rounded p-2 bg-pena-400'>
-          {
-            icon === "cursor" ? <Play /> : 
-            icon === "pen" ? <PenTool /> : 
-            icon === "terminal" ? <Terminal /> : 
-            icon === "user" ? <Users /> : 
-            icon === "bold" ? <Bolt /> : 
-            icon === "arch" ? <Pyramid /> : 
-            icon === "start" ? <BadgeCheck /> : <Pointer />
-          }
+          <div className='rounded p-2 bg-pena-400 text-white'>
+            {' '}
+            {/* Added text-white for white icon */}
+            {getIconComponent(icon)}
           </div>
           <h2 className='ml-4 text-base text-pena-900 dark:text-pena-500'>
-            <Link href={href}>
-              {/* <span className='absolute -inset-px rounded-xl' /> */}
-              {title}
-            </Link>
+            <Link href={href}>{title}</Link>
           </h2>
         </div>
         <p className='mt-1 text-sm text-slate-700 dark:text-slate-400'>
